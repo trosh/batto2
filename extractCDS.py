5d44eb9ff9f866807d4d7f68493d05541787355f
John Gliksberg <jg.trosh@gmail.com>
2016-01-19 16:06:25 +0100

    deal with overwriting. clean dead code.

--- extractCDS.py
+++ extractCDS.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from sys import argv
+import os
 
 if len(argv) < 3:
     print("usage: extractCDS query reference_in [...]")
@@ -10,15 +11,20 @@ with open(argv[1]) as queryfaa:
     query = queryfaa.readline().strip()[1:].split(' ')[0].lower()
 
 with open(argv[2]) as refinfaa:
-    found = False
-    refout_filename = ""
     for line in refinfaa:
         if line[0] == '>':
             cdsname = line.split('|')[2]
             if query in cdsname.lower():
-                found = True
                 refout_filename = cdsname.split(' ')[0] + ".faa"
-                print("writing to "+refout_filename)
+                if os.path.isfile(refout_filename):
+                    question = "overwrite "+refout_filename+" ? [Yn]"
+                    answer = input(question).lower()
+                    if len(answer) > 0 and answer[0] != "y":
+                        print("disregarding "+refout_filename+", continuing")
+                        continue
+                    print("overwriting "+refout_filename)
+                else:
+                    print("writing to "+refout_filename)
                 with open(refout_filename, "w") as refoutfaa:
                     refoutfaa.write(line) # print cds header
                     # keep on printing lines until next cds
