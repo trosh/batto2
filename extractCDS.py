b98ecf62c0ae42919af1aa67e01c185b5c5059d2
John Gliksberg <jg.trosh@gmail.com>
2016-01-19 16:36:22 +0100

    take query as string instead of filename, take multiple references, use genindex instead of first word of cds name for filename

--- extractCDS.py
+++ extractCDS.py
@@ -4,34 +4,36 @@ from sys import argv
 import os
 
 if len(argv) < 3:
-    print("usage: extractCDS query reference_in [...]")
+    print("usage: extractCDS query input_reference_filenames")
     exit(1)
 
-with open(argv[1]) as queryfaa:
-    query = queryfaa.readline().strip()[1:].split(' ')[0].lower()
+query = argv[1].strip().lower()
 
-with open(argv[2]) as refinfaa:
-    for line in refinfaa:
-        if line[0] == '>':
-            cdsname = line.split('|')[2]
-            if query in cdsname.lower():
-                refout_filename = cdsname.split(' ')[0] + ".faa"
-                if os.path.isfile(refout_filename):
-                    question = "overwrite "+refout_filename+" ? [Yn]"
-                    answer = input(question).lower()
-                    if len(answer) > 0 and answer[0] != "y":
-                        print("disregarding "+refout_filename+", continuing")
-                        continue
-                    print("overwriting "+refout_filename)
-                else:
-                    print("writing to "+refout_filename)
-                with open(refout_filename, "w") as refoutfaa:
-                    refoutfaa.write(line) # print cds header
-                    # keep on printing lines until next cds
-                    while True:
-                        line = refinfaa.readline()
-                        if line[0] == '>':
-                            break
-                        refoutfaa.write(line)
-                break
+refins = argv[2:]
+
+for refin_filename in refins:
+    with open(refin_filename) as refinfaa:
+        for line in refinfaa:
+            if line[0] == '>':
+                words = line.split('|')
+                if query in words[2].lower():
+                    refout_filename = words[1] + ".faa"
+                    if os.path.isfile(refout_filename):
+                        question = "overwrite "+refout_filename+" ? [Yn]"
+                        answer = input(question).lower()
+                        if len(answer) > 0 and answer[0] != "y":
+                            print("disregarding "+refout_filename+", continuing")
+                            continue
+                        print("overwriting "+refout_filename)
+                    else:
+                        print("writing to "+refout_filename)
+                    with open(refout_filename, "w") as refoutfaa:
+                        refoutfaa.write(line) # print cds header
+                        # keep on printing lines until next cds
+                        while True:
+                            line = refinfaa.readline()
+                            if line[0] == '>':
+                                break
+                            refoutfaa.write(line)
+                    break
 
