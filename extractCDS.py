51b3e032787b463692bc01aabe405f6485027326
Bouhrour Stéphane <stephane_bouhrour@hotmail.com>
2016-01-19 15:30:33 +0100

    Write to correct file instead of stdout

--- extractCDS.py
+++ extractCDS.py
@@ -10,14 +10,21 @@ with open(argv[1]) as queryfaa:
     query = queryfaa.readline().strip()[1:].split(' ')[0].lower()
 
 with open(argv[2]) as refinfaa:
+    found = False
+    refout_filename = ""
     for line in refinfaa:
         if line[0] == '>':
             cdsname = line.split('|')[2]
             if query in cdsname.lower():
-                print(line, end='') # print cds header
+                found = True
+                refout_filename = cdsname.split(' ')[0]
+                with open(refout_filename+".faa", "w") as refout:
+                    refout.write(line) # print cds header
                 break
-    for line in refinfaa: # keep on printing lines until next cds
-        if line[0] == '>':
-            break
-        print(line, end='')
+    if found:
+        with open(refout_filename+".faa", "a") as refout:
+            for line in refinfaa: # keep on printing lines until next cds
+                if line[0] == '>':
+                    break
+                print(line, end='')
 
